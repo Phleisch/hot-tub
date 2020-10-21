9533b3d01e18d8eaabe87044e34eb348df567b57
Martin Schuck <martin.schuck@tum.de>
2020-10-21 13:33:53 +0200

    doc update

--- src/model_service/model_service.py
+++ src/model_service/model_service.py
@@ -1,3 +1,10 @@
+"""!@brief Interface class to load the temperature difference model.
+@details Matrix is always of size (900, 1800). Matrix element (0,0) lies in the top left corner.
+@file model_service.py ModelService class file.
+@author Martin Schuck
+@date 21.10.2020
+"""
+
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -6,12 +13,25 @@ from reference_model.reference_model_loader import ReferenceModelLoader
 
 
 class ModelService:
+    """!@brief This class is responsible for loading the global temperature difference model.
+
+    Main functionality lies in the @ref load_model function. Plotting of models is only supported for debugging.
+    """
+    ROOT_PATH = Path(__file__).resolve().parents[2]
 
     def __init__(self):
-        self.ROOT_PATH = Path(__file__).resolve().parents[2]
+        """!@brief Standard constructor.
+        """
         self.reference_model_loader = ReferenceModelLoader()
 
     def load_model(self, day, plot_model=False):
+        """!@brief Loads the global reference temperature matrix from the cache.
+
+        @param day Day of the year to load (from 0 to 364). Type int.
+        @param plot_model Flag to plot the model. Plot is given as a seperate matplotlib plot window. Type bool.
+        @return Returns the global temperature difference matrix of shape (900,1800).
+        """
+
         reference_model = self.reference_model_loader.load_model(day)
         try:
             print(self.ROOT_PATH.joinpath('data', 'current_model.npy'))
