4cd44830a3e077a70075fb41e653263fca5d521d
Kai Fleischman <kaifleischmans@gmail.com>
2020-10-21 12:58:21 +0200

    add routing for about page

--- src/webserver/app.py
+++ src/webserver/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 template_image_location = 'static/icon.png'
 
+# How requests for the route (127.0.0.1:5000) should be handled
 @app.route('/')
-def index(temperature_map=template_image_location):
-	return render_template('index.html', temperature_map=temperature_map)
+def index():
+	return render_template('index.html', temperature_map=template_image_location)
 
+# How requests for the route (127.0.0.1:5000/about) should be handled
+@app.route('/about')
+def about():
+	return render_template('about.html')
